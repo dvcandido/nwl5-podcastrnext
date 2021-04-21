89a893c5627244ca6b40e58201e50d4e0756a9b3
Douglas Candido <dgxplusplus@gmail.com>
2021-04-21 00:48:42 -0300

    Criação do Projeto com as aulas 01 e 02

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+export default function Home(props) {
+  return (
+    <div>
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+//SSR
+// export async function getServerSideProps() {
+//   const response = await fetch("http://localhost:3333/episodes");
+//   const data = await response.json();
+
+//   return {
+//     props : {
+//       episodes : data,
+//     }
+//   }
+// }
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+}
